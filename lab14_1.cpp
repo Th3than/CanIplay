222a5e9c9be3f1bb5f5757878e9ced29c5f32ab5
Natan <Th3than@gmail.com>
2023-01-13 15:47:05 +0700

    lab14-1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,42 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double r[]){
+    double all = 0;
+    for (int i = 0; i < n; i++)
+    {
+        all += a[i];
+    }
+    r[0] = all/n;
+   
+    double all2 = 1,all4 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        all2 *= a[i];
+        all4 += pow((a[i]-r[0]),2);
+    }
+    r[1] = sqrt(all4/n);
+    r[2] = pow(all2,1.0/n);
+    double all3 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        all3 += 1/a[i];
+    }
+    r[3] = n/all3;
+    double hi ,low ;
+    hi = a[0];
+    low = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > hi)
+        {
+            hi = a[i];
+        }else if (a[i] < low)
+        {
+            low = a[i];
+        }
+    }
+    r[4] = hi;
+    r[5] = low;
+}
